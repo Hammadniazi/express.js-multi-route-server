d1a5d44e179bfed9aa1d7fb4e4e409e032a42f1c
Hammad-Niazi <hammadniazi991@gmail.com>
2025-09-04 00:33:17 +0200

    create setup and installation for server through express.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express'); 
+const app = express(); 
+const port = 5000; 
+ 
+// This is our route handler for GET requests to the root 
+app.get('/', (req, res) => { 
+  res.send('Welcome to my Server!!'); 
+}); 
+app.get('/about', (req, res) => { 
+  res.send('This is a simple server created to practise Express routing.'); 
+}); 
+ 
+// This starts the server 
+app.listen(port, () => { 
+  console.log(`Server is running and listening on http://localhost:${port}`); 
+}); 
